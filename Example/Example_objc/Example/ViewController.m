4c581c55524fcf235e32dcb195ea3848fd98a73b
Eric McConkie <eric@ericmcconkie.com>
2017-04-18 13:05:46 -0700

    Obj C example with pairing and authJson request/callback

--- Example/Example_objc/Example/ViewController.m
+++ Example/Example_objc/Example/ViewController.m
@@ -19,8 +19,8 @@
     
     
     
-    PoyntPOSConnectionManager *manager = [[PoyntPOSConnectionManager alloc] init];
-    [manager setUrl:@"10.0.1.75:55555"];
+    __block PoyntPOSConnectionManager *manager = [[PoyntPOSConnectionManager alloc] init];
+    [manager setUrl:@"10.0.1.22:55555"];
     [manager setTimeout:6000];
     [manager setClientName:@"bugsnagger"];
     [manager setOnTransactionResponse:^(PoyntTransactionResponseObject *data, PoyntActionType type){
@@ -31,8 +31,21 @@
         NSLog(@"ERROR! %@",error);
     }];
     
-    [manager authorizePairing:@"Q5X7GZ"];
-    // Do any additional setup after loading the view, typically from a nib.
+    [manager attemptPairing:^(BOOL flag, NSError *err) {
+        if(flag){//success!
+            NSString *poyntRequestId    = [[NSUUID UUID] UUIDString];
+            NSString *refId             = [[NSUUID UUID] UUIDString];
+            NSString *orderId           = [[NSUUID UUID] UUIDString];
+            NSString *json =[NSString stringWithFormat:@"{\"timeout\":60000,\"poyntRequestId\":\"%@\",\"payment\":{\"currency\":\"USD\",\"referenceId\":\"%@\",\"orderId\":\"%@\",\"amount\":1000,\"tipAmount\":0,\"cashbackAmount\":0,\"disableDebitCards\":false,\"disableCash\":false,\"debit\":false,\"disableTip\":false,\"skipReceiptScreen\":false,\"cashOnly\":false,\"nonReferencedCredit\":false,\"multiTender\":false}}",poyntRequestId,refId,orderId];
+            [manager authorizeJson:json endPoint:@"devices/cat/authorizeSales" callback:^(id responseData, NSError *error) {
+                
+                NSError *jsonError = nil;
+                NSDictionary * json = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableLeaves error:&jsonError];
+                NSString *reqId = [json objectForKey:@"poyntRequestId" ];
+                NSLog(@"response for request has id: %@\r\n%@",reqId,json);
+            }];
+        }
+    }];
 }
 
 
